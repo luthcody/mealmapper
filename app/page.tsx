b392f3bf6b63928a000d4f2c37887078e388a69e
Cody Luth <39610702+luthcody@users.noreply.github.com>
2024-03-18 22:33:40 -0700

    Initialized meal mapper

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import MainBanner from "@/components/homepage/mainBanner";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-white">
+      <MainBanner />
+    </main>
+  );
+}
